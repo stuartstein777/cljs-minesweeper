6fb5c96f2754fb45594a5214b1da941a78e68a40
Stuart Stein <qmstuart@gmail.com>
2022-07-13 15:11:35 +0100

    Generating board

--- src/main/exfn/logic.cljs
+++ src/main/exfn/logic.cljs
@@ -1,1 +1,36 @@
 (ns exfn.logic)
+
+(defn get-neighbour-mine-count [board [x y]]
+  (let [neighbour-coords [[-1 1] [0 1] [1 1] [-1 0] [1 0] [-1 -1] [0 -1] [1 -1]]]
+    (->> neighbour-coords
+         (map (fn [[xd yd]] (get-in board [(+ x xd) (+ y yd)])))
+         (filter (fn [b] (= b :mine)))
+         (count))))
+
+(defn- generate-board [{:keys [dimensions mines]}]
+  (let [[width height] dimensions]
+    (->>
+     (concat (repeat mines :mine)
+             (repeat (- (* width height) mines) :blank))
+     (shuffle)
+     (partition width)
+     (map vec)
+     vec)))
+
+(defn reducer [acc xy]
+  (if (= (get-in acc xy) :mine)
+    acc
+    (assoc-in acc xy (get-neighbour-mine-count acc xy))))
+
+(defn get-all-cell-coords [board]
+  (for [xs (range (count (first board)))
+        ys (range (count board))]
+    [xs ys]))
+
+(defn add-mine-counts [board cell-coords]
+  (reduce reducer board cell-coords))
+
+(defn generate-full-board [board-options]
+  (let [board (generate-board board-options)
+        cell-coords (get-all-cell-coords board)]
+    (add-mine-counts board cell-coords)))
--- src/test/minesweeper_tests.cljs
+++ src/test/minesweeper_tests.cljs
@@ -1,1 +1,60 @@
-(ns minesweeper-tests)
+(ns minesweeper-tests
+  (:require [cljs.test :refer [deftest is testing run-tests]]
+            [exfn.logic :as lc :refer [add-mine-counts get-all-cell-coords]]))
+
+(deftest test-generating-mine-counts
+  (testing "surrounded"
+    (prn "testing surrounded")
+    (let [board [[:mine :mine  :mine]
+                 [:mine :blank :mine]
+                 [:mine :mine  :mine]]
+          cells (get-all-cell-coords board)]
+      (is (= (add-mine-counts board cells)
+             [[:mine :mine  :mine]
+              [:mine 8 :mine]
+              [:mine :mine  :mine]]))
+      ))
+  (testing "checkerboard"
+    (prn "testing checkerboard")
+    (let [board [[:mine :blank  :mine :blank :mine]
+                 [:blank  :mine :blank :mine :blank]
+                 [:mine :blank  :mine :blank :mine]
+                 [:blank  :mine :blank :mine :blank]
+                 [:mine :blank  :mine :blank :mine]]
+          cells (get-all-cell-coords board)]
+      (is (= (add-mine-counts board cells)
+             [[:mine 3  :mine 3 :mine]
+              [3  :mine 4 :mine 3]
+              [:mine 4  :mine 4 :mine]
+              [3 :mine 4 :mine 3]
+              [:mine 3  :mine 3 :mine]]))))
+  (testing "sparse"
+    (prn "testing sparse")
+    (let [board [[:blank  :blank  :blank :blank :blank]
+                 [:blank  :blank  :blank :mine  :blank]
+                 [:blank  :blank  :mine  :blank :blank]
+                 [:blank  :blank  :blank :mine  :blank]
+                 [:mine   :blank  :blank :blank :blank]]
+          cells (get-all-cell-coords board)]
+      (is (= (add-mine-counts board cells)
+             [[0  0  1 1 1]
+              [0  1  2 :mine  1]
+              [0  1  :mine  3 2]
+              [1  2  2 :mine  1]
+              [:mine  1  1 1 1]]))))
+  
+  (testing "very sparse"
+    (prn "very sparse")
+    (let [board [[:blank  :blank  :blank :blank :blank]
+                 [:blank  :blank  :blank :blank :blank]
+                 [:blank  :blank  :mine  :blank :blank]
+                 [:blank  :blank  :blank :blank :blank]
+                 [:blank  :blank  :blank :blank :blank]]
+          cells (get-all-cell-coords board)]
+      (is (= (add-mine-counts board cells)
+             [[0  0  0 0 0]
+              [0  1  1 1 0]
+              [0  1  :mine  1 0]
+              [0  1  1 1  0]
+              [0  0  0 0 0]]))))
+  )
